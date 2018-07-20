71703af64571bcaa968bc8cb65484ca2e5bdd5bb
Drew Arrigoni <arrigoni.andrew@gmail.com>
2018-07-20 18:36:40 -0400

    Fix fixture issue; Update DB; Integration test fix

--- features/support/fixtures.rb
+++ features/support/fixtures.rb
@@ -7,9 +7,12 @@ require 'active_support'
 ActiveRecord::Base.establish_connection({ "adapter"=>"postgresql",
                                           "encoding"=>"unicode",
                                           "pool"=>"5",
-                                          "database"=>"drew-server_test"})
+                                          "database"=>"drew-app_test"})
 
 Before do
-
+  ActiveRecord::FixtureSet.reset_cache
+  fixtures_folder = File.join('./features/support/fixtures/')
+  fixtures = Dir[File.join(fixtures_folder, '*.yml')].map {|f| File.basename(f, '.yml') }
+  ActiveRecord::FixtureSet.create_fixtures(fixtures_folder, fixtures)
 end
 
