9b3adffe721bcbc92ee7d4a2596634d216c305e7
Drew Arrigoni <arrigoni.andrew@gmail.com>
2018-08-12 20:51:59 -0400

    Debug info for cucumber failure in CI

--- features/support/fixtures.rb
+++ features/support/fixtures.rb
@@ -2,6 +2,9 @@ def load_fixture(fixture_path='default')
   ActiveRecord::FixtureSet.reset_cache
   fixtures_folder = File.join("./features/support/fixtures/#{fixture_path}/")
   fixtures = Dir[File.join(fixtures_folder, '*.yml')].map {|f| File.basename(f, '.yml') }
-  ActiveRecord::FixtureSet.create_fixtures(fixtures_folder, fixtures)
+  results = ActiveRecord::FixtureSet.create_fixtures(fixtures_folder, fixtures)
+
+  puts results.first.size
+  puts ActiveRecord::Base.connection.execute('SELECT COUNT(*) FROM tasks')
 end
 
