66321b5213cbae3f8ed5941b7b7b3ae56d4dcf2c
Drew Arrigoni <arrigoni.andrew@gmail.com>
2019-03-17 13:07:51 -0400

    Chromedriver update/fix

--- features/support/capybara.rb
+++ features/support/capybara.rb
@@ -1,7 +1,10 @@
 require 'capybara/cucumber'
+require 'chromedriver-helper'
 
 Capybara.register_driver :chrome do |app|
-  Capybara::Selenium::Driver.new(app, browser: :chrome)
+  Capybara::Selenium::Driver.new(app, browser: :chrome,
+    driver_opts: { log_path: 'out/chromedriver.out' }
+  )
 end
 
 Capybara.default_driver = :chrome
